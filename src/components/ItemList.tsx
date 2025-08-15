857cc45f16a1f1380abe351ecbcb06cc1279ef23
renanmartins1813 <renanmartins1813@gmail.com>
2025-08-14 23:17:08 -0500

    sorting and typos

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,5 +1,7 @@
+import Select, { type SingleValue } from "react-select"
 import { useItemsContext } from "../lib/hooks"
 import EmptyView from "./EmptyView"
+import { useMemo, useState } from "react"
 
 type ItemProps = {
     id: number,
@@ -20,15 +22,54 @@ function Item({ name, id, checked }: ItemProps) {
         </li>
     )
 }
+type SortOption = {
+    label: string;
+    value: "default" | "checked" | "unchecked";
+};
+
+const sortingOptions: SortOption[] = [
+    { label: 'Sort by default', value: 'default' },
+    { label: 'Sort by checked', value: 'checked' },
+    { label: 'Sort by unchecked', value: 'unchecked' }
+];
 
 export default function ItemList() {
     const { items } = useItemsContext()
+    const [sortBy, setSortBy] = useState("default");
+
+    const sortedItems = useMemo(() => {
+        return (
+            [...items].sort((a, b) => {
+                if (sortBy === 'checked') {
+                    return Number(b.checked) - Number(a.checked)
+                }
+                if (sortBy === 'unchecked') {
+                    return Number(a.checked) - Number(b.checked)
+                }
+
+                return 0
+            })
+        )
+
+    }, [items, sortBy])
 
     return (
         <ul className="item-list">
             {items.length === 0 && <EmptyView />}
 
-            {items.map((item) => {
+            {items.length > 0 && <section className="sorting">
+                <Select
+                    options={sortingOptions}
+                    defaultValue={sortingOptions[0]}
+                    onChange={(option: SingleValue<SortOption>) => {
+                        if (option) {
+                            setSortBy(option.value)
+                        }
+                    }}
+                />
+            </section>}
+
+            {sortedItems.map((item) => {
                 return <Item key={item.id} name={item.name} id={item.id} checked={item.checked} />
             })}
         </ul>
